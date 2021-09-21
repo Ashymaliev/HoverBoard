cff11e75c79393b6e12799a089e9459aecb36931
Ashymaliev <Nursultan-99kg@mail.ru>
2021-09-21 19:06:32 +0600

    add js color for squares

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const board =document.querySelector('#board')
+const colors =['#e74c3c','#8e44ad', '#3498db',
+'#e67e22', '#2ecc71']
 const SQUARES_NUMBER = 500
 
 for (let i =0; i <SQUARES_NUMBER; i++) {
@@ -15,7 +17,19 @@ for (let i =0; i <SQUARES_NUMBER; i++) {
 }
 
 function setColor(element) {
-    element.style.backgroundColor = 'red'
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 
+    0 0 10px ${color}`
 }
 
-function removeColor(element)
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.
+    length)
+    return colors[index]
+}
