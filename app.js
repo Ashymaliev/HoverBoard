15ffe2f7fca306a7d2bc10a295292b7e4271d239
Ashymaliev <Nursultan-99kg@mail.ru>
2021-09-21 18:54:35 +0600

    add function for mouse

--- app.js
+++ app.js
@@ -5,5 +5,17 @@ for (let i =0; i <SQUARES_NUMBER; i++) {
     const square =document.createElement('div')
     square.classList.add('square')
 
+    square.addEventListener('mouseover', () =>
+    setColor (square))
+
+    square.addEventListener('mouseleave', () =>
+    removeColor (square))
+
     board.append(square)
 }
+
+function setColor(element) {
+    element.style.backgroundColor = 'red'
+}
+
+function removeColor(element)
