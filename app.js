cc3d619211f13f378908bd906e98dbafa9784572
Ashymaliev <Nursultan-99kg@mail.ru>
2021-09-21 18:45:38 +0600

    add js board

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ const SQUARES_NUMBER = 500
 for (let i =0; i <SQUARES_NUMBER; i++) {
     const square =document.createElement('div')
     square.classList.add('square')
+
+    board.append(square)
 }
